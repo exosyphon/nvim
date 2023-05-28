53a0972ba604e7106bd6d5869eb2098564cd9c60
Andrew Courter <exosyphon@yahoo.com>
2023-05-28 14:23:42 -0500

    Add additional languages to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,17 +9,26 @@ lsp.ensure_installed({
 	'ruby_ls',
 	'standardrb',
 	'kotlin_language_server',
+    'jdtls',
 	'lua_ls',
-	'jsonls'
+	'jsonls',
+    'html',
+    'elixirls',
+    'tailwindcss',
+    'tflint',
+    'pylsp',
+    'dockerls',
+    'bashls',
+    'marksman'
 })
 
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<C-y>"] = cmp.mapping.confirm({select = true}),
-    	['<C-Space>'] = cmp.mapping.complete(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({select = true}),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
