beef63e507972ac25b46d5e18e38986e4ea5d070
Andrew Courter <exosyphon@yahoo.com>
2023-06-10 11:27:18 -0500

    Update colors config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,1 @@
-function Colors(color)
-	color = color or "gruvbox"
-	vim.cmd.colorscheme = color
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-Colors()
-
 vim.cmd('colorscheme gruvbox')
--- lua/exosyphon/packer.lua
+++ lua/exosyphon/packer.lua
@@ -11,117 +11,111 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use({ "ellisonleao/gruvbox.nvim", 
-    as = "gruvbox", 
-    config = function()
-        vim.cmd('colorscheme gruvbox')
-    end
-})
+    use { "ellisonleao/gruvbox.nvim" }
 
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use('nvim-treesitter/playground')
-use('ThePrimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {                                      -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-}
-}
-
-use("nathom/tmux.nvim")
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
-use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-}
-use {
-  "nvim-neotest/neotest",
-  requires = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "antoinemadec/FixCursorHold.nvim",
-    "olimorris/neotest-rspec",
-    "haydenmeade/neotest-jest",
-    "zidhuss/neotest-minitest",
-    "nvim-neotest/neotest-vim-test",
-  },
-}
-use 'mfussenegger/nvim-dap'
-use 'rcarriga/nvim-dap-ui'
-use 'mxsdev/nvim-dap-vscode-js'
-use 'suketa/nvim-dap-ruby'
-use 'Pocco81/DAPInstall.nvim'
-use 'rcarriga/cmp-dap'
-use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-})
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
-use { 'junegunn/fzf', run = ":call fzf#install()" }
-use 'nanotee/zoxide.vim'
-use {'nvim-telescope/telescope-ui-select.nvim' }
-use "debugloop/telescope-undo.nvim"
-use {
-  "AckslD/nvim-neoclip.lua",
-  requires = {
-    {'nvim-telescope/telescope.nvim'},
-  },
-}
-use 'theHamsta/nvim-dap-virtual-text'
-use 'andymass/vim-matchup'
-use 'jinh0/eyeliner.nvim'
-use { "anuvyklack/windows.nvim",
-   requires = {
-      "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim"
-   },
-   config = function()
-      vim.o.winwidth = 10
-      vim.o.winminwidth = 10
-      vim.o.equalalways = false
-      require('windows').setup()
-   end
-}
-use 'voldikss/vim-floaterm'
-use {
-    'tummetott/unimpaired.nvim',
-    config = function()
-        require('unimpaired').setup {
-            -- add any options here or leave empty
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
         }
-    end
-}
-use 'airblade/vim-gitgutter'
-use 'mg979/vim-visual-multi'
+    }
+
+    use("nathom/tmux.nvim")
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    use {
+        "nvim-neotest/neotest",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "olimorris/neotest-rspec",
+            "haydenmeade/neotest-jest",
+            "zidhuss/neotest-minitest",
+            "nvim-neotest/neotest-vim-test",
+        },
+    }
+    use 'mfussenegger/nvim-dap'
+    use 'rcarriga/nvim-dap-ui'
+    use 'mxsdev/nvim-dap-vscode-js'
+    use 'suketa/nvim-dap-ruby'
+    use 'Pocco81/DAPInstall.nvim'
+    use 'rcarriga/cmp-dap'
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use { 'junegunn/fzf', run = ":call fzf#install()" }
+    use 'nanotee/zoxide.vim'
+    use {'nvim-telescope/telescope-ui-select.nvim' }
+    use "debugloop/telescope-undo.nvim"
+    use {
+        "AckslD/nvim-neoclip.lua",
+        requires = {
+            {'nvim-telescope/telescope.nvim'},
+        },
+    }
+    use 'theHamsta/nvim-dap-virtual-text'
+    use 'andymass/vim-matchup'
+    use 'jinh0/eyeliner.nvim'
+    use { "anuvyklack/windows.nvim",
+        requires = {
+            "anuvyklack/middleclass",
+            "anuvyklack/animation.nvim"
+        },
+        config = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = false
+            require('windows').setup()
+        end
+    }
+    use 'voldikss/vim-floaterm'
+    use {
+        'tummetott/unimpaired.nvim',
+        config = function()
+            require('unimpaired').setup {
+                -- add any options here or leave empty
+            }
+        end
+    }
+    use 'airblade/vim-gitgutter'
+    use 'mg979/vim-visual-multi'
 end)
 
