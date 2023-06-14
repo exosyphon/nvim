77c725e258f69451853c952c49616897eebd6d4f
Andrew Courter <exosyphon@yahoo.com>
2023-06-14 07:47:51 -0500

    Add shortcut for searching references

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,6 +38,7 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
