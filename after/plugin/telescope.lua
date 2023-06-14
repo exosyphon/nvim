5ebef9422c838149ef19e48061f97535232326b2
exosyphon <exosyphon@yahoo.com>
2023-06-13 19:10:28 -0500

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fc', '<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>')
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
