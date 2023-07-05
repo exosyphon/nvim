5cfbcf030cd31706fa64a6ddce3d815445e8e3bf
exosyphon <exosyphon@yahoo.com>
2023-07-05 18:30:22 -0500

    Update remap.lua

--- lua/exosyphon/remap.lua
+++ lua/exosyphon/remap.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+vim.keymap.set("v", "//", 'y/<C-R>"<CR>')
+
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
