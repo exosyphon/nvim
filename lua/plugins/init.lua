64f9f7e3d9f5ad14f83216e9c2241be62dd28ed9
exosyphon <exosyphon@yahoo.com>
2023-07-11 12:49:02 -0500

    Update neotest config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,7 +26,41 @@ return {
             "olimorris/neotest-rspec",
             "haydenmeade/neotest-jest",
             "zidhuss/neotest-minitest",
+            "mfussenegger/nvim-dap",
         },
+        config = function ()
+            local neotest = require('neotest')
+
+            local neotest_jest = require('neotest-jest')({
+                jestCommand = 'npm test --'
+            })
+            neotest_jest.filter_dir = function (name)
+                return name ~= 'node_modules' and name ~= '__snapshots__'
+            end
+
+            neotest.setup({
+                adapters = {
+                    require('neotest-rspec')({
+                        rspec_cmd = function()
+                            return vim.tbl_flatten({
+                                "bundle",
+                                "exec",
+                                "rspec",
+                            })
+                        end
+                    }),
+                    require('neotest-minitest'),
+                    neotest_jest,
+                },
+                output_panel = {
+                    enabled = true,
+                    open = "botright split | resize 15"
+                },
+                quickfix = {
+                    open = false,
+                }
+            })
+        end
     },
     {
         "mfussenegger/nvim-dap",
