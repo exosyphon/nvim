fcb39ada1c65929d6b35ae37a62aaf283ed14870
exosyphon <exosyphon@yahoo.com>
2023-07-11 12:46:23 -0500

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,32 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        highlight = {
-            enable = true,
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-    }
+        configs.setup({
+            ensure_installed = {
+                "javascript",
+                "typescript",
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "elixir",
+                "erlang",
+                "java",
+                "kotlin",
+                "jq",
+                "dockerfile",
+                "json",
+                "html",
+                "terraform",
+                "go",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
