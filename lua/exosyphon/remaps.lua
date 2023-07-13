bc1424af14c43d65c5e2413d9e544bf35470bdf2
exosyphon <exosyphon@yahoo.com>
2023-07-13 11:53:16 -0500

    Update remaps.lua

--- lua/exosyphon/remaps.lua
+++ lua/exosyphon/remaps.lua
@@ -26,8 +26,8 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>q", "<cmd>bd<CR>")
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>h", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>;", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
