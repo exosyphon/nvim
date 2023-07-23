3c1c465c121eb748b3317a12f73cb39de2c2c7ab
exosyphon <exosyphon@yahoo.com>
2023-07-23 07:47:38 -0500

    Remove lspkind

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -321,7 +321,6 @@ local plugins = {
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
             { "rafamadriz/friendly-snippets" },
-            { "onsails/lspkind.nvim" },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
             { 'hrsh7th/cmp-cmdline' },
