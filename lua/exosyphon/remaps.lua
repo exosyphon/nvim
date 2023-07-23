d2ca42f5f846818389b4f107878b818875d01461
exosyphon <exosyphon@yahoo.com>
2023-07-23 07:32:14 -0500

    Use C-S-Left/Right for resizing vim windows

--- lua/exosyphon/remaps.lua
+++ lua/exosyphon/remaps.lua
@@ -92,8 +92,8 @@ end)
 vim.keymap.set("n",  "<leader>Z", "<cmd>Zi<CR>")
 
 -- Resize with arrows
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-S-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-S-Up>", ":resize -2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
