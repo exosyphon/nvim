348d724854c814b851b4e36439ae26857fa113e5
Andrew Courter <exosyphon@yahoo.com>
2023-07-28 08:08:50 -0400

    Add Octo as a plugin

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -16,6 +16,17 @@ vim.g.maplocalleader = ' '
 
 local plugins = {
     {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup()
+        end
+    },
+    {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
         dependencies = { { 'nvim-lua/plenary.nvim' } }
