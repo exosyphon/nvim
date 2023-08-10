fa3359bb98de2851a15aaa0c1f26d3aa357e2454
exosyphon <exosyphon@yahoo.com>
2023-08-10 13:21:32 -0500

    Fix incorrect float term remap

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -297,7 +297,7 @@ local plugins = {
         'voldikss/vim-floaterm',
         config = function()
             vim.keymap.set("n", "<leader>ft",
-                "<cmd>:FloatermToggle --height=0.7 --width=0.8 --wintype=float --name=floaterm1 --position=center --autoclose=2<CR>", { desc = "Open FloatTerm"})
+                "<cmd>:FloatermNew --height=0.7 --width=0.8 --wintype=float --name=floaterm1 --position=center --autoclose=2<CR>", { desc = "Open FloatTerm"})
             vim.keymap.set("n", "<leader>flt",
                 "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm"})
             vim.keymap.set("t", "<leader>flt",
