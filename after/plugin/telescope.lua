0059368f318680d3ac2536a1172a0607a961332e
exosyphon <exosyphon@yahoo.com>
2023-09-15 17:25:37 -0500

    Update telescope.lua with multi select

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -23,6 +23,21 @@ table.insert(vimgrep_arguments, "!**/.git/*")
 
 local actions = require "telescope.actions"
 
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup({
     defaults = {
         -- `hidden = true` is not supported in text grep commands.
@@ -35,7 +50,7 @@ telescope.setup({
             i = {
                 ["<C-j>"] = actions.cycle_history_next,
                 ["<C-k>"] = actions.cycle_history_prev,
-
+                ["<CR>"] = select_one_or_multi,
                 ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
             }
         },
