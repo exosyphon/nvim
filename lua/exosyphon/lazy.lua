7370bbbc34644e110f31a4a82131cb41371707d9
Andrew Courter <exosyphon@yahoo.com>
2023-10-11 22:09:53 -0500

    Add treesitter incremental selection

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -103,6 +103,15 @@ local plugins = {
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = "<C-space>",
+                        node_decremental = "<bs>",
+                    }
+                }
             })
         end
     },
