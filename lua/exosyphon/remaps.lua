efcba3a830735d619506db9edd88c87d1ccd8418
exosyphon <exosyphon@yahoo.com>
2023-11-16 08:39:19 -0600

    Fix TermOpen autocommand to not trigger on neotest

--- lua/exosyphon/remaps.lua
+++ lua/exosyphon/remaps.lua
@@ -130,9 +130,17 @@ vim.api.nvim_create_augroup("custom_buffer", { clear = true })
 
 -- start terminal in insert mode
 vim.api.nvim_create_autocmd("TermOpen", {
-  group   = "custom_buffer",
+  desc = "Auto enter insert mode when opening a terminal",
+  group = "custom_buffer",
   pattern = "*",
-  command = "startinsert | set winfixheight"
+  callback = function()
+    -- Wait briefly just in case we immediately switch out of the buffer (e.g. Neotest)
+    vim.defer_fn(function()
+      if vim.api.nvim_buf_get_option(0, 'buftype') == 'terminal' then
+        vim.cmd([[startinsert]])
+      end
+    end, 100)
+  end,
 })
 
 -- highlight yanks
