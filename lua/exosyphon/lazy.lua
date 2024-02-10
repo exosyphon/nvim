dd61d369c07a453bd7cdeb0ac9f58dbcc279a5e8
Andrew Courter <exosyphon@yahoo.com>
2024-02-10 12:29:20 -0600

    Add "nvim-treesitter/nvim-treesitter-textobjects"

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -256,6 +256,7 @@ local plugins = {
     lazy = true,
     event = "VeryLazy",
   },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -297,6 +298,50 @@ local plugins = {
             node_incremental = "<C-space>",
             scope_incremental = "<C-CR>",
             node_decremental = "<bs>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>p"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>ps"] = "@parameter.inner",
+            },
           },
         },
       })
