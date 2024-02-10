e267f0d4c0148293fa8e5c57698f86d90fe3635b
Andrew Courter <exosyphon@yahoo.com>
2024-02-10 12:09:21 -0600

    Increase timeout for conform to 1 second

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -189,7 +189,7 @@ local plugins = {
         conform.format({
           lsp_fallback = true,
           async = false,
-          timeout_ms = 500,
+          timeout_ms = 1000,
         })
       end, { desc = "Format file or range (in visual mode)" })
     end,
