557bb20775b49f4666a56a9e4e3bd25771f0a66b
exosyphon <exosyphon@yahoo.com>
2024-02-16 23:21:00 -0600

    Update sync url for Obsidian

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -2,7 +2,7 @@ require("obsidian").setup({
   workspaces = {
     {
       name = "ObsidianVault",
-      path = "/Users/andrew/ObsidianVault",
+      path = "/Users/andrew/Google Drive/My Drive/ObsidianVault",
     },
   },
   completion = {
