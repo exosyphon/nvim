2c275382a2ead9d4e0e160e7251eb191f92cf28d
exosyphon <exosyphon@yahoo.com>
2024-02-21 13:21:58 -0600

    Add gx.nvim plugin

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -16,6 +16,17 @@ vim.g.maplocalleader = " "
 
 local plugins = {
   {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,      -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
+  },
+  {
     'stevearc/oil.nvim',
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
