15fb24d3ccca7d95012c545a5152fc651a14b282
exosyphon <exosyphon@yahoo.com>
2024-03-21 11:25:00 -0500

    Show path for lualine_c

--- lua/exosyphon/lazy.lua
+++ lua/exosyphon/lazy.lua
@@ -595,7 +595,7 @@ local plugins = {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "diff", "diagnostics" },
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
