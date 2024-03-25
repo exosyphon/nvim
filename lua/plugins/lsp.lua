8e898ebea4c45c0b7bd0f0b95a61c6173185be65
exosyphon <exosyphon@yahoo.com>
2024-03-25 11:32:38 -0500

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -123,6 +123,11 @@ return {
     })
 
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
       sources = {
         { name = "nvim_lsp" },
         { name = "luasnip", keyword_length = 2 },
