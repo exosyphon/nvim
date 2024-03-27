c4ac8b87a7dd72ed53772db583eaa73418799c28
exosyphon <exosyphon@yahoo.com>
2024-03-27 08:27:08 -0500

    Update obsidian.lua config

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -19,6 +19,20 @@ return {
         min_chars = 2,
       },
       new_notes_location = "notes_subdir",
+      note_id_func = function(title)
+        return title
+      end,
+      note_frontmatter_func = function(note)
+        local out = { id = note.id, aliases = {}, tags = note.tags }
+
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
       mappings = {},
 
       templates = {
