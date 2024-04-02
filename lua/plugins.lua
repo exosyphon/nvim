1c968ff92ab77aa7a034c8c8dcbf87a30d55faea
exosyphon <exosyphon@yahoo.com>
2024-04-02 11:30:29 -0500

    Add Neotree plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,25 @@
  return {
+   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = { "Neotree" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true,
+          },
+        },
+      })
+    end,
+  },
   {
     "David-Kunz/gen.nvim",
     opts = {
