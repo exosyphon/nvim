05ebe0420034fbdbd860371e4af7396466bf3426
Andrew Courter <exosyphon@yahoo.com>
2024-06-03 20:47:58 -0500

    Upgrade plugins to latest

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,7 +32,7 @@ return {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "mistral",
+      model = "llama3",
       display_mode = "float",
     }
   },
@@ -288,7 +288,7 @@ return {
     -- stylua: ignore
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
