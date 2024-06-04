e25545d7426b07b4e2d75a9001abb61cce1087c0
Andrew Courter <exosyphon@yahoo.com>
2024-06-03 21:13:27 -0500

    Add additional configuration options for oil nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,6 +67,8 @@ return {
     config = function()
       require("oil").setup({
         default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
       })
     end,
   },
