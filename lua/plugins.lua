14bc3c850d887ad4e4c35f4d67053c971276d0ce
Andrew Courter <exosyphon@yahoo.com>
2024-06-03 22:25:53 -0500

    More configuration for oil nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -69,6 +69,13 @@ return {
         default_file_explorer = true,
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+        }
       })
     end,
   },
