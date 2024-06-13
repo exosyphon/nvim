525694635d0b16f7d461f516b5c8b1a95381c861
exosyphon <exosyphon@yahoo.com>
2024-06-13 09:54:11 -0500

    Customize Oil float window

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -73,12 +73,22 @@ return {
           show_hidden = true,
           natural_order = true,
           is_always_hidden = function(name, _)
-            return name == '..' or name == '.git'
+            return name == ".." or name == ".git"
           end,
+        },
+        float = {
+          padding = 2,
+          max_width = 90,
+          max_height = 0,
         },
         win_options = {
           wrap = true,
-        }
+          winblend = 0,
+        },
+        keymaps = {
+          ["<C-c>"] = false,
+          ["q"] = "actions.close",
+        },
       })
     end,
   },
