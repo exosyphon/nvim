c5060b34c1c835cdb9885426c8279573b61a2425
Andrew Courter <exosyphon@yahoo.com>
2024-07-02 07:52:38 -0500

    Rendering working with patch inside local config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -99,7 +99,7 @@ return {
           use_delta = true,
           use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
           side_by_side = false,
-          diff_context_lines = vim.o.scrolloff,
+          vim_diff_opts = { ctxlen = vim.o.scrolloff },
           entry_format = "state #$ID, $STAT, $TIME",
           mappings = {
             i = {
