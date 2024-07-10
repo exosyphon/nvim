e406346ee27294684d7d9152f40ddd9e25857309
Andrew Courter <exosyphon@yahoo.com>
2024-07-10 09:44:01 -0500

    Remove gx and update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -93,17 +93,6 @@ return {
   },
   { "mfussenegger/nvim-jdtls" },
   {
-    "chrishrb/gx.nvim",
-    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
-    cmd = { "Browse" },
-    init = function()
-      vim.g.netrw_nogx = 1 -- disable netrw gx
-    end,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = true,      -- default settings
-    submodules = false, -- not needed, submodules are required only for tests
-  },
-  {
     "stevearc/oil.nvim",
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
