fac9db425529063ac128fd80f4a7eb53aaf9da4c
Andrew Courter <exosyphon@yahoo.com>
2024-12-15 07:17:34 -0600

    Update to split out image.nvim configuration and add note to README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Neovim Configuration
 
 Have fun!
+
+## Installation Notes
+If you want to have image.nvim installed you must have imagemagick installed on your machine first.
+
+If you'd like to skip this then remove the file plugins/image.lua
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,52 +1,10 @@
 return {
   {
-    'vhyrro/luarocks.nvim',
-    priority = 1001, -- this plugin needs to run before anything else
-    opts = {
-      rocks = { 'magick' },
-    },
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
   },
   { 'folke/neodev.nvim', opts = {} },
-  {
-    "3rd/image.nvim",
-    config = function()
-      require("image").setup({
-        backend = "kitty",
-        integrations = {
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = true,
-            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-            resolve_image_path = function(document_path, image_path, fallback)
-              return fallback(document_path, image_path)
-            end,
-          },
-          html = {
-            enabled = false,
-          },
-          css = {
-            enabled = false,
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-      })
-    end,
-  },
   {
     "chentoast/marks.nvim",
     config = function()
