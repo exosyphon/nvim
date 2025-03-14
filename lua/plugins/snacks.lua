dd9e0c9cf503164279c557c50fd56282e0758c61
Andrew Courter <andrew.courter@arrivehealth.com>
2025-03-14 08:03:27 -0500

    Update snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,10 +10,21 @@ return {
     input = { enabled = false },
     picker = { enabled = false },
     notifier = { enabled = false },
-    quickfile = { enabled = false },
+    quickfile = { enabled = true },
     scope = { enabled = false },
-    scroll = { enabled = false },
+    scroll = { enabled = true },
     statuscolumn = { enabled = false },
     words = { enabled = false },
-  }
+    rename = { enabled = true },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if event.data.actions.type == "move" then
+          Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+      end,
+    })
+  end
 }
