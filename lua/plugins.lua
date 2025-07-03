c8d8527da12953dd2384f427453ebaa48c05b285
Andrew Courter <andrew.courter@arrivehealth.com>
2025-07-03 14:56:59 -0500

    Revert "Add lazy loading"
    This reverts commit a5cbc0cb780b5e1d80b58d198d83c431b342886a.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,18 +5,17 @@ return {
     ft = { "markdown", "codecompanion" },
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
   },
-  { 'folke/neodev.nvim', opts = {}, lazy = true },
+  { 'folke/neodev.nvim', opts = {} },
   {
     "chentoast/marks.nvim",
-    lazy = true,
     config = function()
       require("marks").setup({
         default_mappings = true,
       })
     end,
   },
-  { 'brenoprata10/nvim-highlight-colors', lazy = true },
-  { "tpope/vim-projectionist", lazy = true },
+  { 'brenoprata10/nvim-highlight-colors' },
+  { "tpope/vim-projectionist" },
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -45,14 +44,12 @@ return {
       "sindrets/diffview.nvim", -- optional - Diff integration
     },
     config = true,
-    lazy = true,
   },
-  { "mfussenegger/nvim-jdtls", lazy = true },
+  { "mfussenegger/nvim-jdtls" },
   {
     "stevearc/oil.nvim",
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = true,
     config = function()
       require("oil").setup({
         default_file_explorer = true,
@@ -81,7 +78,7 @@ return {
       })
     end,
   },
-  { "preservim/vim-pencil", lazy = true },
+  { "preservim/vim-pencil" },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
@@ -190,7 +187,6 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
-    lazy = true,
     config = function()
       require("treesitter-context").setup({
         max_lines = 5,
@@ -199,7 +195,6 @@ return {
   },
   {
     "RRethy/vim-illuminate",
-    lazy = true,
     config = function()
       require("illuminate")
     end,
@@ -257,7 +252,7 @@ return {
     lazy = true,
     event = "VeryLazy",
   },
-  { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -350,7 +345,7 @@ return {
       })
     end,
   },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
   {
     "folke/tokyonight.nvim",
     opts = {
@@ -369,36 +364,31 @@ return {
   },
   {
     "exosyphon/telescope-color-picker.nvim",
-    lazy = true,
     config = function()
       vim.keymap.set("n", "<leader>uC", "<cmd>Telescope colors<CR>", { desc = "Telescope Color Picker" })
     end,
   },
   {
     "mbbill/undotree",
-    lazy = true,
     config = function()
       vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<CR>", { desc = "Telescope Undo" })
     end,
   },
   {
     "tpope/vim-fugitive",
-    lazy = true,
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open Fugitive Panel" })
     end,
   },
-  { "tpope/vim-repeat", lazy = true },
+  "tpope/vim-repeat",
   {
     "numToStr/Comment.nvim",
-    lazy = true,
     config = function()
       require("Comment").setup()
     end,
   },
   {
     "windwp/nvim-autopairs",
-    lazy = true,
     config = function()
       require("nvim-autopairs").setup()
     end,
@@ -418,7 +408,6 @@ return {
       "nvim-neotest/neotest-go",
     },
     opts = {},
-    lazy = true,
     config = function()
       local neotest = require("neotest")
 
@@ -457,7 +446,6 @@ return {
   },
   {
     "mfussenegger/nvim-dap",
-    lazy = true,
     dependencies = {
       {
         "rcarriga/nvim-dap-ui",
@@ -547,14 +535,12 @@ return {
   {
     "junegunn/fzf",
     build = ":call fzf#install()",
-    lazy = true,
   },
-  { "nanotee/zoxide.vim", lazy = true },
-  { "nvim-telescope/telescope-ui-select.nvim", lazy = true },
-  { "debugloop/telescope-undo.nvim", lazy = true },
+  "nanotee/zoxide.vim",
+  "nvim-telescope/telescope-ui-select.nvim",
+  "debugloop/telescope-undo.nvim",
   {
     "voldikss/vim-floaterm",
-    lazy = true,
     config = function()
       vim.keymap.set(
         "n",
@@ -568,14 +554,12 @@ return {
   },
   {
     "tummetott/unimpaired.nvim",
-    lazy = true,
     config = function()
       require("unimpaired").setup()
     end,
   },
   {
     "lewis6991/gitsigns.nvim",
-    lazy = true,
     config = function()
       require("gitsigns").setup({
         on_attach = function(bufnr)
@@ -637,14 +621,13 @@ return {
       })
     end,
   },
-  { "mg979/vim-visual-multi", lazy = true },
-  { "tpope/vim-rails", lazy = true },
+  "mg979/vim-visual-multi",
+  "tpope/vim-rails",
   {
     "williamboman/mason.nvim",
     dependencies = {
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    lazy = true,
     config = function()
       local mason = require("mason")
       local mason_tool_installer = require("mason-tool-installer")
@@ -695,10 +678,9 @@ return {
       },
     },
   },
-  { "nvim-telescope/telescope-live-grep-args.nvim", lazy = true },
+  { "nvim-telescope/telescope-live-grep-args.nvim" },
   {
     "aaronhallaert/advanced-git-search.nvim",
-    lazy = true,
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "tpope/vim-fugitive",
@@ -710,7 +692,6 @@ return {
     dependencies = {
       "ravitemer/mcphub.nvim"
     },
-    lazy = true,
     config = function()
       require("codecompanion").setup({
         strategies = {
