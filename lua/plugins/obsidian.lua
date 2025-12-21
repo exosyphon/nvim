6a89b3c7963e3a8b272a17ea42117b153cd61d90
Andrew Courter <exosyphon@yahoo.com>
2025-12-21 09:49:09 -0600

    Update obsidian and plugins

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,54 +6,52 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  config = function()
-    require("obsidian").setup({
-      workspaces = {
-        {
-          name = "ObsidianVault",
-          path = "/Users/andrew.courter/Documents/ObsidianVault",
-        },
+  opts = {
+    workspaces = {
+      {
+        name = "ObsidianVault",
+        path = "~/Documents/ObsidianVault",
       },
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-      new_notes_location = "notes_subdir",
-      note_id_func = function(title)
-        return title
-      end,
-      frontmatter = {
-        func = function(note)
-          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    new_notes_location = "notes_subdir",
+    note_id_func = function(title)
+      return title
+    end,
+    frontmatter = {
+      func = function(note)
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
 
-          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-            for k, v in pairs(note.metadata) do
-              out[k] = v
-            end
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
           end
+        end
 
-          return out
+        return out
+      end,
+    },
+    templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      tags = "",
+      substitutions = {
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+        tomorrow = function()
+          return os.date("%Y-%m-%d", os.time() + 86400)
         end,
       },
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M",
-        tags = "",
-        substitutions = {
-          yesterday = function()
-            return os.date("%Y-%m-%d", os.time() - 86400)
-          end,
-          tomorrow = function()
-            return os.date("%Y-%m-%d", os.time() + 86400)
-          end,
-        },
-      },
+    },
 
-      ui = {
-        enable = false, -- using render-markdown.nvim instead
-      },
-      legacy_commands = false,
-    })
-  end,
+    ui = {
+      enable = false,   -- using render-markdown.nvim instead
+    },
+    legacy_commands = false,
+  }
 }
