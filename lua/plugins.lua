50ca182759d695fda547af416f87af8c3c9fb2c4
exosyphon <exosyphon@yahoo.com>
2025-12-21 09:37:43 -0600

    Add mcphub as explicit dependency

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -734,5 +734,59 @@ return {
       vim.keymap.set({ "n", "v" }, "<leader>ct", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
       vim.keymap.set({ "v" }, "<leader>ca", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
     end,
+   },
+   {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    event = "VeryLazy",
+    config = function()
+      require("mcphub").setup({
+        --- `mcp-hub` binary related options-------------------
+        config = vim.fn.expand("~/.config/mcphub/servers.json"), -- Absolute path to MCP Servers config file (will create if not exists)
+        port = 37373,                                            -- The port `mcp-hub` server listens to
+        shutdown_delay = 60 * 10 * 000,                          -- Delay in ms before shutting down the server when last instance closes (default: 10 minutes)
+        use_bundled_binary = false,                              -- Use local `mcp-hub` binary (set this to true when using build = "bundled_build.lua")
+        mcp_request_timeout = 60000,                             --Max time allowed for a MCP tool or resource to execute in milliseconds, set longer for long running tasks
+
+        ---Chat-plugin related options-----------------
+        auto_approve = false,           -- Auto approve mcp tool calls
+        auto_toggle_mcp_servers = true, -- Let LLMs start and stop MCP servers automatically
+
+        --- Plugin specific options-------------------
+        native_servers = {}, -- add your custom lua native servers here
+        builtin_tools = {
+          edit_file = {
+            parser = {
+              track_issues = true,
+              extract_inline_content = true,
+            },
+            locator = {
+              fuzzy_threshold = 0.8,
+              enable_fuzzy_matching = true,
+            },
+            ui = {
+              go_to_origin_on_complete = true,
+              keybindings = {
+                accept = ".",
+                reject = ",",
+                next = "n",
+                prev = "p",
+                accept_all = "ga",
+                reject_all = "gr",
+              },
+            },
+          },
+        },
+        log = {
+          level = vim.log.levels.WARN,
+          to_file = false,
+          file_path = nil,
+          prefix = "MCPHub",
+        },
+      })
+    end
   }
 }
